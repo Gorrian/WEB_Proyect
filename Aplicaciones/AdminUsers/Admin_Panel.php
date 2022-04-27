94148357f103dc7a357203358ca34c771c30ae0f
Gorrian <gorriansan@hotmail.com>
2022-04-27 15:09:36 +0200

    Problemas resultos con Admin panel
    Se han resuelto algunos problemas a la hora de imprimir la tabla con la informacion de los usuarios.

--- Aplicaciones/AdminUsers/Admin_Panel.php
+++ Aplicaciones/AdminUsers/Admin_Panel.php
@@ -1,7 +1,29 @@
+<head>
+    <meta charset="UTF-16"/>
+</head>
+
 <?php
     require "../Con_Database/Conexion.php";
     session_start();
     
+    function IsChecked (int $Value){
+        if($Value==1){
+            return "checked";
+        }
+        return "";
+    }
+    function FormConv($Index, $Value, $ID){
+        switch ($Index){
+            case "DNI":
+            case "Contrasena":
+                $Return="<input type='Submit' name='$Index' value='$Value'/>
+                        <input type='Hidden' name='ID' value='$ID'/>";
+            case  "Change_password" || "Disabled" || "Admin":
+                $Return="<input type='checkbox' name='$Index' ".IsChecked($Value)."/>";
+        }
+    }
+
+
     if(isset($_SESSION['DNI'])){
         $Conexion=Con_Database(GetScheme("../Scheme.txt"));
         $SQL="SELECT Admin from Trabajadores WHERE DNI='".$_SESSION['DNI']."' AND Admin=1";
@@ -12,28 +34,26 @@
                 $SQL="SELECT * FROM ADMIN_PANEL";
                 $Trabajadores=$Conexion->query($SQL);
                 $HEAD=true;
-                $HTML=<<<HERE
-                <table border="1">
-                HERE;
+                $HTML='<table border="1">';
                 while($ROW=$Trabajadores->fetch_assoc()){
-                    $HTML+=<<<HERE
-                        <tr>
-                    HERE;
-                    foreach($ROW as $Index->$Value){
-                        if($HEAD){
-                            $HTML+=<<<HERE
-                                <th>$Index</th>
-                            HERE;
+                    $HTML.='<tr>';
+                    if($HEAD){
+                        $HTML.="<tr>";
+                        foreach($ROW as $Index=>$Value){
+                            $HTML.="<th>$Index</th>";
                         }
-                        $HTML+=<<<HERE
-                            <td>$Value</td>
-                        HERE;
+                        $HTML.="</tr>";
+                    }
+                    foreach($ROW as $Index=>$Value){
+                        
+                        
+                        $HTML.="<td>$Value</td>";
                     }
                     $HEAD=false;
-                    $HTML+=<<<HERE
-                        </tr>
-                    HERE;
+                    $HTML.='</tr>';
                 }
+                $HTML.='</table>';
+                print($HTML);
             }else{
                 header("Location: /Index.php");
             }
