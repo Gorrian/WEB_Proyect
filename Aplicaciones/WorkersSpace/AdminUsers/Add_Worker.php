84c42a395a5f320e7ea2b533aa62d588edfca2cd
Gorrian <gorriansan@hotmail.com>
2022-05-12 17:59:18 +0200

    Mas progreso sobre Add_Worker
    El formulario ya esta hecho, habria que retocar las validaciones y el procesado de informacion un poco mas para dar mas informacion al usuario sobre que deberia introducir en caso de error.

--- Aplicaciones/WorkersSpace/AdminUsers/Add_Worker.php
+++ Aplicaciones/WorkersSpace/AdminUsers/Add_Worker.php
@@ -1,19 +1,38 @@
+<head>
+    <script>
+     
+    </script>
+</head>
+
 <?php
 require "../../Con_Database/Conexion.php";
 require "../../Con_Database/SQL_Protection.php";
 session_start();
+
+
 
 if(isset($_SESSION['DNI'])){
     $Conexion=Con_Database(GetScheme("../../Scheme.txt"));
     if($Conexion->connect_errno){
         die("Error de Conexion (".$Conexion->connect_errno.") ". $Conexion->connect_error);
     }else{
+        if(isset($_POST['submit'])){
+            $_POST=SQLProtection($_POST);
+            if($_POST['FirstPassword']==$_POST['ConfirmPassword']){
+                $SQL="INSERT INTO TRABAJADORES (DNI, `Nombre completo`, `ID departamento`, `Telefono`, `C. electronico`, Localidad, `Password`)
+                 VALUES ('".$_POST['DNI']."','".$_POST['NombreCompleto']."', '".$_POST['Departamento']."','".$_POST['Telefono']."','".$_POST['CorreoElectronico']."', '".$_POST['Localidad']."', PASSWORD('".$_POST['FirstPassword']."'))";
+                 $Conexion->query($SQL);
+            }else{
+                print("<h3 class='Error'>La contraseña establecida y la repetida no concuerdan</h3>");
+            }
+        }
         $SQL="SELECT * FROM `trabajadores` WHERE DNI='".$_SESSION['DNI']."' AND (`ID departamento`=1 or `ID departamento`=3 or Admin=1)";
         if($Conexion->query($SQL)->num_rows==1){
-            print("<form>");
+            print_r("");
+            print("<form method='post' action='".$_SERVER['PHP_SELF']."'>");
             print<<<HERE
                 <label>DNI Trabajador</label><br/>
-                <input type="text" name="DNI" required/><br/>
+                <input type="text" name="DNI" pattern="[0-9]{8}[A-Za-z]{1}" required/><br/>
                 <label>Nombre Completo</label><br/>
                 <input type="text" name="NombreCompleto" required/><br/>
                 <label>Departamento</label><br/>
@@ -44,7 +63,7 @@ if(isset($_SESSION['DNI'])){
                 <input type="password" name="FirstPassword" required/><br/>
                 <label>Repetir contraseña</label><br/>
                 <input type="password" name="ConfirmPassword" required/><br/>
-                <input type="submit" name="submit" value="Añadir"/>
+                <input type="submit" name="submit" value="Enviar"/>
                 <input type="reset"/>
             HERE;
             print("</form>");
