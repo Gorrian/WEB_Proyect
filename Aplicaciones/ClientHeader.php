d52e93e07e074dc7e4fce5da9552693b52c2c56d
Gorrian <gorriansan@hotmail.com>
2022-05-25 14:02:52 +0200

    Header Pagina de clientes
    Empezado el header que se incluira en las paginas de los clientes.

---
+++ Aplicaciones/ClientHeader.php
@@ -0,0 +1,18 @@
+<header>
+    <nav>
+        <ul>
+            <li>
+                Inicio
+            </li>
+            <li>
+                Hacer una peticion
+            </li>
+            <li>
+                Espacio de trabajadores
+            </li>
+        </ul>
+    </nav>
+    <div>
+        <iframe src="/Aplicaciones/ClientApps/Login.php"></iframe>
+    </div>
+</header>
